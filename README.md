7413ed672f391771554c817f75d9517ea3d1ca6e
Aaron_Y <amyerke@uncc.edu>
2019-06-10 09:40:35 -0400

    developing a plan

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # toyDataSet
 scripts and files related to building and running a toy dataset for testing BioLockJ
+
+
+The plan is to take three humans samples, one from each of the three experimental groups: GWL, PWL, and NSC.
+
+#SampleID	BarcodeSequence	LinkerPrimerSequence	Barcode	Barcode_ID	Species	Sex	Mouse_human_ID	Weightgain_time3	Baseline_weight	Weightgain_week1	weightgain_week2	weightgain_week3	weightgain_week4	Weightgain_time4	Weight_prior_WD	Mouse_GLP1_baseline	Mouse_GLP1_1week	Mouse_GLP1_4week	Timepoint_code	ID_code	Time_Point	Group	Donor	Group_Donor	Diet_post_WD_phase	Cohort	Date_of_fecal_collection	Human_GLP1	Human_GLP2	Human_PYY	Human_CA	Human_DCA	Human_CDCA	Human_TB	Description	Num_Reads	Num_Trimmed_Reads	Num_Valid_Reads
+195	CTGAAGGGCGAA	AATGATACGGCGACCACCGAGATCTACACGCCTCCCTCGCGCCATCAGAGATGTG	806rcbc195	195	Human	F	12H	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	MBP	12H-MBP	Human	GWL	MBP12	GWL12	NA	NA	NA	36.07	13.47	644.8	2.554166667	8.888333333	8.856666667	20.29916667	12H-MBP	67258	67258	67258
+196	CGCTCACAGAAT	AATGATACGGCGACCACCGAGATCTACACGCCTCCCTCGCGCCATCAGAGATGTG	806rcbc196	196	Human	F	13H	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	MBP	13H-MBP	Human	PWL	MBP13	PWL13	NA	NA	NA	33.85	14.79	582.32	13.69166667	15.4375	37.24166667	66.37083333	13H-MBP	81630	81630	81630
+198	CGAGCTGTTACC	AATGATACGGCGACCACCGAGATCTACACGCCTCCCTCGCGCCATCAGAGATGTG	806rcbc198	198	Human	F	15H	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	NA	MBP	15H-MBP	Human	Ctrl	MBP15	CTRL15	NA	NA	NA	10.29	7.9	337.52	9.203333333	1.975833333	9.950833333	21.115H-MBP	33625	33625	33625
+
+
--- reduceFastqSamples.js
+++ reduceFastqSamples.js
@@ -1,19 +1,87 @@
+/**
+ * Title: Fastq parser for selecting experimental groups and rarifying data
+ * Author: Aaron Yerke
+ * Start date: June 2019
+ * Notes:
+ * grep --color 'CGAGCTGTTACC' headR1.fastq
+ * scp amyerke@hpc.uncc.edu:/users/amyerke/toydataset/gastricToy/test1/headR1.txt .
+ */
+
+"use strict"
+ //import libraries
 const fs = require('fs'),
   path = require('path'),
   readline = require('readline');
 
+//take 1 out of X sequences
+const rarify = 200;
+
+const keeperBarCodes = [
+  'CTGAAGGGCGAA',
+  'CGCTCACAGAAT',
+  'CGAGCTGTTACC',
+  'TATGTGCCGGCT',
+  'TGGTCGCATCGT',
+  'TGTAAGACTTGG',
+  'CGGATCTAGTGT',
+  'CGATCTTCGAGC',
+  'GTCGAATTTGCG',
+  'CTGGAAATCTGCA'
+]
+
+//Create place to write new sequence files
+const output = fs.createWriteStream('renameOutput.fastq', { flags: 'a' });
+
+/**
+ * Read through the file, add selected barcodes to output
+ * @param {*} inputFile 
+ */
 function processFile(inputFile) {
   let instream = fs.createReadStream(inputFile),
     outstream = new (require('stream'))(),
     rl = readline.createInterface(instream, outstream);
 
   rl.on('line', function (line) {
-    console.log(line);
+    if (line.startsWith('@M0') && barcodeCheck(line)) {
+      console.log(line);
+      //output.write(line)
+    }
   });
 
   rl.on('close', function (line) {
-    console.log(line);
+    console.log("rl.on('close': ", line);
     console.log('done reading file.');
   });
 }
-processFile(path.join('.', 'headGastricR1.fastq'));
+
+/**
+ * Test if line ends in one of our selected barcodes
+ * @param {*} line 
+ * 
+ *   for (const bc of keeperBarCodes) {
+    if (line.endsWith(bc)) {
+        return(true)
+      }else{
+        return(false)
+      }
+  }
+ */
+function barcodeCheck(line) {
+  // for (let bc = 0; bc < keeperBarCodes.length; bc++) {
+  for (const element of keeperBarCodes) {
+    // const element = keeperBarCodes[bc];
+    if (line.endsWith(element)) {
+      return true;
+    }
+  }
+  return false;
+  // keeperBarCodes.forEach( bc => {
+  //   if (line.endsWith(bc)) {
+  //     return(true)
+  //   };
+  // })
+  // return(false)
+}
+
+
+processFile(path.join('.', 'headR1.fastq'));
